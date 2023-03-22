56824441cc361b919e55d5dc584d7facd89a4b51
declanyg <declan@youngs.asia>
2023-03-22 02:02:06 -0400

    Finished Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,10 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Declan Young <span className="sm:block hidden">| Personal Portfolio</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Declan Young &nbsp;
+            <span className="sm:block hidden">| &nbsp;Portfolio</span>
+            </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map( (link) => (
