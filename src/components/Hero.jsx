6c8fabb939aa1182f018791710cd8eb67b35c9cb
declanyg <declan@youngs.asia>
2023-04-18 10:23:37 -0400

    Changing colours

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,14 @@ const Hero = () => {
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='w-5 h-5 rounded-full bg-[#377ef7]' />
+          <div className='w-1 sm:h-80 h-40 blue-gradient'/>
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Declan</span>
+            {/* Hi, I'm <span className="text-[#915eff]">Declan</span> */}
+            Hi, I'm <span className="text-[#377ef7]">Declan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a CS major at Boston University <br className="sm:block hidden"/>interested in full stack development and AI.
